76d6302c70bf0e3a689c5385ad7512630559e73c
marinaaglic <marinaaglic11@gmail.com>
2023-12-11 18:08:05 +0100

    added registration page

---
+++ frontend/src/pages/RegistrationPage.tsx
@@ -0,0 +1,11 @@
+import RegistrationForm from "../components/forms/RegistrationForm";
+
+function RegistrationPage() {
+  return (
+    <div>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default RegistrationPage;
