5a1cc8566cea84d6cce43f31bc4f4f56809e48dd
marinaaglic <marinaaglic11@gmail.com>
2023-12-11 16:54:26 +0100

    small fix

--- frontend/src/components/forms/UserRegistrationForm.tsx
+++ frontend/src/components/forms/UserRegistrationForm.tsx
@@ -6,7 +6,7 @@ export default function UserRegistrationForm() {
   return (
     <FormWrapper title="User Details">
       <form>
-        <Input type="text" label="Name" id="name" />
+        <Input type="text" label="First Name" id="name" />
         <Input type="text" label="Last Name" id="lastName" />
         <Input type="email" label="E-mail" id="email" />
         <Input type="password" label="Password" id="password" />
