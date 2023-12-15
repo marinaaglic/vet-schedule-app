c3c98606ae1c85c6b548500bb344a96dfe38ac87
marinaaglic <marinaaglic11@gmail.com>
2023-12-15 19:21:37 +0100

    registration implemented on frontend

--- frontend/src/components/forms/PetRegistrationForm.tsx
+++ frontend/src/components/forms/PetRegistrationForm.tsx
@@ -1,14 +1,44 @@
 import Input from "../reusable/Input";
 import FormWrapper from "./FormWrapper";
 import "../../styles/_registrationForms.scss";
+import { Pet } from "../../types/pet";
+import { SetStateAction, ChangeEvent, Dispatch } from "react";
 
-export default function PetRegistrationForm() {
+interface PetRegistrationFormProps {
+  setPetData: Dispatch<SetStateAction<Pet>>;
+}
+
+export default function PetRegistrationForm({
+  setPetData,
+}: PetRegistrationFormProps) {
+  function changeHandler(
+    field: keyof Pet,
+    event: ChangeEvent<HTMLInputElement>
+  ) {
+    const value = event.target.value;
+    setPetData((prevData) => ({ ...prevData, [field]: value }));
+  }
   return (
     <FormWrapper title="Pet Details">
       <div>
-        <Input type="text" label="Pet's Name" id="name" />
-        <Input type="text" label="Type of Pet" id="type" />
-        <Input type="text" label="Breed" id="breed" />
+        <Input
+          type="text"
+          label="Pet's Name"
+          id="name"
+          onChange={(value) => changeHandler("name", value)}
+        />
+        <Input
+          type="text"
+          label="Type of Pet"
+          id="type"
+          onChange={(value) => changeHandler("type", value)}
+        />
+        <Input
+          type="text"
+          label="Breed"
+          id="breed"
+          onChange={(value) => changeHandler("breed", value)}
+        />
       </div>
     </FormWrapper>
   );
--- frontend/src/components/forms/RegistrationForm.tsx
+++ frontend/src/components/forms/RegistrationForm.tsx
@@ -1,18 +1,54 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { useMultiStepForm } from "../../hooks/useMultiStepForm";
 import UserRegistrationForm from "./UserRegistrationForm";
 import PetRegistrationForm from "./PetRegistrationForm";
 import Sidebar from "../reusable/Sidebar";
+import AuthService from "../../services/AuthService";
 import "../../styles/_registrationForms.scss";
 import "../../styles/_sidebar.scss";
+import { Pet } from "../../types/pet";
+import { User } from "../../types/user";
+import { useNavigate } from "react-router";
 
 export default function RegistrationForm() {
-  const { steps, currentStep, step, isFirstStep, isLastStep, back, next } =
-    useMultiStepForm([<UserRegistrationForm />, <PetRegistrationForm />]);
+  const [userData, setUserData] = useState<User>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    role: "owner" as const,
+  });
 
-  function onSubmitHandler(e: FormEvent) {
-    e.preventDefault();
-    next();
+  const [petData, setPetData] = useState<Pet>({
+    name: "",
+    type: "",
+    breed: "",
+    owner: "",
+  });
+  const navigate = useNavigate();
+  const { steps, currentStep, step, isFirstStep, isLastStep, back, next } =
+    useMultiStepForm([
+      <UserRegistrationForm setUserData={setUserData} />,
+      <PetRegistrationForm setPetData={setPetData} />,
+    ]);
+
+  async function onSubmitHandler(event: FormEvent) {
+    event.preventDefault();
+    if (isLastStep) {
+      try {
+        const userResponse = await AuthService.register(userData);
+        const userId = userResponse.userId;
+        const petDataWithOwner = { ...petData, owner: userId };
+        await AuthService.registerPet(userId, petDataWithOwner);
+
+        console.log("User and pet registration successful!");
+        navigate("/home");
+      } catch (error) {
+        console.log("Registration failed: ", error);
+      }
+    } else {
+      next();
+    }
   }
 
   return (
