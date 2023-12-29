1bb5783da1f9de5178827e136d9b653b0926a36e
Marina Aglic <magliccuv@pmfst.hr>
2023-12-29 23:11:25 +0100

    added logout handler and saving to local storage

--- frontend/src/components/navbars/AuthenticatedNavBar.tsx
+++ frontend/src/components/navbars/AuthenticatedNavBar.tsx
@@ -1,7 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../../styles/_authenticatedNavBar.scss";
+import AuthService from "../../services/AuthService";
+import { useContext } from "react";
+import { AuthContext, AuthContextProps } from "../../context/AuthContext";
 
 export default function AuthenticatedNavBar() {
+  const navigate = useNavigate();
+  const { setAuthenticated } = useContext<AuthContextProps>(AuthContext);
+
+  const logoutHandler = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        console.log("Token not found.");
+        return;
+      }
+      await AuthService.logout(token, setAuthenticated);
+      localStorage.removeItem("token");
+      navigate("/login");
+    } catch (error: any) {
+      console.log("Logout failed: ", error);
+    }
+  };
   return (
     <nav className="auth-navbar">
       <div className="div-links">
@@ -16,9 +36,9 @@ export default function AuthenticatedNavBar() {
         </Link>
       </div>
 
-      <Link to="/logout" className="btn-link">
+      <button className="btn-logout" onClick={logoutHandler}>
         Logout
-      </Link>
+      </button>
     </nav>
   );
 }
--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, Navigate } from "react-router";
 import AuthenticatedNavBar from "../components/navbars/AuthenticatedNavBar";
 import UnauthenticatedNavBar from "../components/navbars/UnauthenticatedNavBar";
 import LoginPage from "../pages/LoginPage";
@@ -6,6 +6,7 @@ import HomePage from "../pages/HomePage";
 import RegistrationPage from "../pages/RegistrationPage";
 import { useContext } from "react";
 import { AuthContext, AuthContextProps } from "../context/AuthContext";
+import PrivateRoute from "../hocs/PrivateRoute";
 
 export default function AppRoutes() {
   const { isAuthenticated } = useContext(AuthContext) as AuthContextProps;
@@ -18,6 +19,22 @@ export default function AppRoutes() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/registration" element={<RegistrationPage />} />
+          <Route
+            path="/profile"
+            element={
+              isAuthenticated ? <PrivateRoute /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              isAuthenticated ? (
+                <PrivateRoute />
+              ) : (
+                <Navigate to="/appointments" />
+              )
+            }
+          />
         </Routes>
       </div>
     </div>
