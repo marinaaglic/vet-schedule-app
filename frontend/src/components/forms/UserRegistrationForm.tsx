0e087783e838319151f794e0cdf6742401cb9da3
marinaaglic <marinaaglic11@gmail.com>
2024-02-09 13:47:03 +0100

    added form validation in the registration forms

--- frontend/src/components/forms/UserRegistrationForm.tsx
+++ frontend/src/components/forms/UserRegistrationForm.tsx
@@ -1,56 +1,131 @@
 import Input from "../reusable/Input";
 import "../../styles/_registrationForms.scss";
 import FormWrapper from "./FormWrapper";
-import { SetStateAction, ChangeEvent, Dispatch } from "react";
+import { SetStateAction, Dispatch } from "react";
 import { User } from "../../types/user";
+import { z } from "zod";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface UserRegistrationFormProps {
   userData: User;
   setUserData: Dispatch<SetStateAction<User>>;
 }
 
+const schema = z.object({
+  firstName: z.string().min(1, "First Name is required."),
+  lastName: z.string().min(1, "Last Name is required."),
+  email: z.string().email("Invalid e-mail."),
+  password: z.string().min(8, "Password must be at least 8 characters long."),
+});
+
 export default function UserRegistrationForm({
   userData,
   setUserData,
 }: UserRegistrationFormProps) {
-  function changeHandler(
-    field: keyof User,
-    event: ChangeEvent<HTMLInputElement>
-  ) {
-    const value = event.target.value;
-    setUserData((prevData) => ({ ...prevData, [field]: value }));
-  }
+  const {
+    formState: { errors },
+    control,
+  } = useForm({
+    resolver: zodResolver(schema),
+    mode: "onBlur",
+    defaultValues: userData,
+  });
   return (
     <FormWrapper title="User Details">
       <div>
-        <Input
-          type="text"
-          label="First Name"
-          id="firstName"
-          value={userData.firstName}
-          onChange={(value) => changeHandler("firstName", value)}
+        <Controller
+          name="firstName"
+          control={control}
+          defaultValue={userData.firstName}
+          render={({ field }) => (
+            <Input
+              type="text"
+              label="First Name"
+              id="firstName"
+              {...field}
+              onChange={(e) => {
+                field.onChange(e);
+                setUserData((prevData) => ({
+                  ...prevData,
+                  firstName: e.target.value,
+                }));
+              }}
+            />
+          )}
         />
-        <Input
-          type="text"
-          label="Last Name"
-          id="lastName"
-          value={userData.lastName}
-          onChange={(value) => changeHandler("lastName", value)}
+        {errors.firstName && (
+          <p className="error">{errors.firstName.message as string}</p>
+        )}
+        <Controller
+          name="lastName"
+          control={control}
+          defaultValue={userData.lastName}
+          render={({ field }) => (
+            <Input
+              type="text"
+              label="Last Name"
+              id="lastName"
+              {...field}
+              onChange={(e) => {
+                field.onChange(e);
+                setUserData((prevData) => ({
+                  ...prevData,
+                  lastName: e.target.value,
+                }));
+              }}
+            />
+          )}
         />
-        <Input
-          type="email"
-          label="E-mail"
-          id="email"
-          value={userData.email}
-          onChange={(value) => changeHandler("email", value)}
+        {errors.lastName && (
+          <p className="error">{errors.lastName.message as string}</p>
+        )}
+        <Controller
+          name="email"
+          control={control}
+          defaultValue={userData.email}
+          render={({ field }) => (
+            <Input
+              type="email"
+              label="E-mail"
+              id="email"
+              {...field}
+              onChange={(e) => {
+                field.onChange(e);
+                setUserData((prevData) => ({
+                  ...prevData,
+                  email: e.target.value,
+                }));
+              }}
+            />
+          )}
         />
-        <Input
-          type="password"
-          label="Password"
-          id="password"
-          value={userData.password}
-          onChange={(value) => changeHandler("password", value)}
+        {errors.email && (
+          <p className="error">{errors.email.message as string}</p>
+        )}
+        <Controller
+          name="password"
+          control={control}
+          defaultValue={userData.password}
+          render={({ field }) => (
+            <Input
+              type="password"
+              label="Password"
+              id="password"
+              {...field}
+              onChange={(e) => {
+                field.onChange(e);
+                setUserData((prevData) => ({
+                  ...prevData,
+                  password: e.target.value,
+                }));
+              }}
+            />
+          )}
         />
+        {errors.password && (
+          <p className="error">{errors.password.message as string}</p>
+        )}
       </div>
     </FormWrapper>
   );
