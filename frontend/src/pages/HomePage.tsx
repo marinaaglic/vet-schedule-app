0658e19bd6f2154528cd04328e5827d6a95fe241
marinaaglic <marinaaglic11@gmail.com>
2024-02-27 16:47:43 +0100

    edited homepage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,36 @@
 import "../styles/_homepage.scss";
+import landingPageImg from "../../public/landing-page-svg.svg";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div className="homepage-div">
-      <h1>Welcome to Vet Clinic</h1>
-      <h2>Book your appointment now!</h2>
+      <div className="div-content">
+        <h1>Welcome to Vet Clinic!</h1>
+        <h2>Book your appointment now.</h2>
+        <p className="sign-in-content">
+          At Vet Clinic you can book your appointments online, edit them if you
+          had a change of plans and cancel them.
+        </p>
+        <Link to="/login" className="link-login-homepage">
+          Sign In
+        </Link>
+        <p className="sign-up-content">
+          Don't have an account? Sign up
+          <Link to="/registration" className="link-registration-homepage">
+            Here
+          </Link>
+          .
+        </p>
+      </div>
+
+      <div className="img-container">
+        <img
+          src={landingPageImg}
+          alt="Landing SVG image."
+          className="dog-img"
+        />
+      </div>
     </div>
   );
 }
